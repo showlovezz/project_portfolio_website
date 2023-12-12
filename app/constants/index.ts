6506d291d006b94449374dc3298993755d784914
vita hsieh <dragic.hsieh@gmail.com>
2023-12-13 02:59:47 +0800

    add navlinks constant

--- app/constants/index.ts
+++ app/constants/index.ts
@@ -1,3 +1,5 @@
+import { RxHome, RxPerson, RxDashboard, RxClipboard } from "react-icons/rx";
+
 export const SOCIALS = [
   {
     name: "Discord",
@@ -10,5 +12,28 @@ export const SOCIALS = [
   {
     name: "Instagram",
     src: "/discord.svg",
+  },
+];
+
+export const NAVLINKS = [
+  {
+    name: "/",
+    icon: RxHome,
+    link: "/",
+  },
+  {
+    name: "/my-skills",
+    icon: RxPerson,
+    link: "/my-skills",
+  },
+  {
+    name: "/my-projects",
+    icon: RxDashboard,
+    link: "/my-projects",
+  },
+  {
+    name: "/contact-me",
+    icon: RxClipboard,
+    link: "/contact-me",
   },
 ];
