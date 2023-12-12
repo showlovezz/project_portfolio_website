013914d69f049d2c75957532a83b9e66c93a08b1
vita hsieh <dragic.hsieh@gmail.com>
2023-12-13 02:02:16 +0800

    add socials constant

---
+++ app/constants/index.ts
@@ -0,0 +1,14 @@
+export const SOCIALS = [
+  {
+    name: "Discord",
+    src: "/instagram.svg",
+  },
+  {
+    name: "Facebook",
+    src: "/facebook.svg",
+  },
+  {
+    name: "Instagram",
+    src: "/discord.svg",
+  },
+];
